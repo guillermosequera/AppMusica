08981fdd5dde7701958dd228beaf8c5f8d6edcc3
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-20 05:31:20 -0300

    getting the letter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Formulario from './components/Formulario';
 
 function App() {
+
+  //definir el State
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+
+  useEffect(() => {
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+
+      guardarLetra(resultado.data.lyrics);
+    }
+    consultarApiLetra();
+
+  }, [busquedaletra]);
+
   return (
       <Fragment>
-        <Formulario/>
+        <Formulario
+          guardarBusquedaLetra={guardarBusquedaLetra}
+        />
       </Fragment>
 
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
 
     const [busqueda, guardarBusqueda] = useState({
         artista: '',
@@ -27,11 +27,15 @@ const Formulario = () => {
             return;
         }
         guardarError(false);
-        
+
+        //todo bien, pasar al componente principal
+        guardarBusquedaLetra(busqueda);
+
     }
 
     return (
         <div className="bg-info">
+            { error ? <p className="alert alert-danger text-center p-2">Todos los campos son obligatorios</p> : null }
             <div className="container">
                 <div className="row">
                     <form
