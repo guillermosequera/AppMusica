8f979fa0018afcd91738ae6ab3851878498a0221
Guillermo Sequera <sequeracrespo@gmail.com>
2020-09-20 06:12:00 -0300

    showing the lyrics of the song

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
 
 function App() {
 
@@ -30,6 +31,19 @@ function App() {
         <Formulario
           guardarBusquedaLetra={guardarBusquedaLetra}
         />
+
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-6">
+
+            </div>
+            <div className="col-md-6">
+              <Cancion 
+                letra={letra}
+              />
+            </div>
+          </div>
+        </div>
       </Fragment>
 
   );
